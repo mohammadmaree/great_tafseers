e0ded7625237434e6700a3989b3702ad6aa83764
Mohammad Maree <mmaree@ITGDOMAIN.ITGSOLUTIONS.com>
2022-05-09 16:50:58 +0300

    update

--- lib/features/home_page/domain/entities/home_page.dart
+++ lib/features/home_page/domain/entities/home_page.dart
@@ -9,7 +9,6 @@ class HomePage extends Equatable {
   const HomePage({required this.nameOfSora, required this.partOfSora,required  this.numberOfPage,required this.imageOfSora});
 
   @override
-  // TODO: implement props
   List<Object?> get props => [nameOfSora,partOfSora,numberOfPage,imageOfSora];
 
 }
